311d00408d9a7fb441e33494ce9ea4d3ada0e805
Bruno Sguerra <bmsguerra@gmail.com>
2023-07-27 15:05:02 +0200

    Create metrics.py

---
+++ metrics.py
@@ -0,0 +1,59 @@
+import math
+import pandas as pd 
+from sklearn.metrics import accuracy_score, recall_score, f1_score
+
+class Eval_metrics(object):
+    def __init__(self):
+        self._subjects = None
+
+    @property
+    def subjects(self):
+        return self._subjects
+
+    @subjects.setter
+    def subjects(self, subjects):
+        """
+        args:
+        subjects: list, [test_users, test_items, test_scores] 
+        """         
+        
+        assert isinstance(subjects, list)
+        test_users, test_items, test_scores, test_y = subjects[0], subjects[1], subjects[2], subjects[3]
+
+        test_set = pd.DataFrame({'user': test_users,
+                                 'item': test_items,
+                                 'score': test_scores,
+                                 'y': test_y})
+        
+        self._subjects = test_set
+
+    def cal_acc(self):
+        "compute accuracy"
+        test_set = self._subjects
+        test_set['pred'] = 0
+        test_set.loc[test_set['score'] >= 0.5, 'pred'] = 1
+        
+        pred = test_set['pred'].values
+        true = test_set['y'].values
+        return accuracy_score(true, pred)
+    
+    def cal_recall(self):
+        "compute recall"
+        test_set = self._subjects
+        test_set['pred'] = 0
+        test_set.loc[test_set['score'] >= 0.5,'pred'] = 1
+        
+        pred = test_set['pred'].values
+        true = test_set['y'].values
+        return recall_score(true, pred)
+       
+    def cal_f1(self):
+        "compute weighted F1 score"
+        test_set = self._subjects
+        test_set['pred'] = 0
+        test_set.loc[test_set['score'] >= 0.5,'pred'] = 1
+        
+        pred = test_set['pred'].values
+        true = test_set['y'].values
+        return f1_score(true, pred,average='weighted')
+        
